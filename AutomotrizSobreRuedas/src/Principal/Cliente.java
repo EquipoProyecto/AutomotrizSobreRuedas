1ca0c7c6f593f7c113e6a59df11a39cbb39f1e20
Vyto <vyto.castro@gmail.com>
2015-11-04 11:31:39 -0300

    clientes
    clase cliente

---
+++ AutomotrizSobreRuedas/src/Principal/Cliente.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Principal;
+
+/**
+ *
+ * @author Vyto
+ */
+public class Cliente {
+    
+    private int _codigo;
+    private String _nombre;
+    private String _aprellido_p;
+    private String _apellido_m;
+    
+}
