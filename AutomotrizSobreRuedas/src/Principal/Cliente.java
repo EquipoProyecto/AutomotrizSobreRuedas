726e6bb9acb9d42801bb78cb9ea5001b5e1da3b2
Vyto <vyto.castro@gmail.com>
2015-11-04 11:35:05 -0300

    version 2 de clients
    clientes 2

--- AutomotrizSobreRuedas/src/Principal/Cliente.java
+++ AutomotrizSobreRuedas/src/Principal/Cliente.java
@@ -15,5 +15,39 @@ public class Cliente {
     private String _nombre;
     private String _aprellido_p;
     private String _apellido_m;
+
+    public int getCodigo() {
+        return _codigo;
+    }
+
+    public void setCodigo(int _codigo) {
+        this._codigo = _codigo;
+    }
+
+    public String getNombre() {
+        return _nombre;
+    }
+
+    public void setNombre(String _nombre) {
+        this._nombre = _nombre;
+    }
+
+    public String getAprellido_p() {
+        return _aprellido_p;
+    }
+
+    public void setAprellido_p(String _aprellido_p) {
+        this._aprellido_p = _aprellido_p;
+    }
+
+    public String getApellido_m() {
+        return _apellido_m;
+    }
+
+    public void setApellido_m(String _apellido_m) {
+        this._apellido_m = _apellido_m;
+    }
+    
+    
     
 }
