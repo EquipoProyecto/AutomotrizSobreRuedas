bfa0ae3f7f08ab048ebc2bc3c86b2028eb2e685c
17783175-1 <17783175-1@pmt_lab0117.inacap.cl>
2015-11-04 11:22:48 -0300

    aqui ta

---
+++ AutomotrizSobreRuedas/src/Conectar/Conectar.java
@@ -0,0 +1,43 @@
+
+package Conectar;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conectar {
+     static String db ="ejemplo";
+    static String login="asd";
+    static String password="asd";
+    static String url="jdbc:derby://localhost:1527/"+db;
+
+    
+    Connection conn = null;
+    
+    public Conectar() 
+    {
+        try {
+         Class.forName("org.apache.derby.jdbc.ClientDriver");
+         conn = DriverManager.getConnection(url,login,password);
+         if(conn != null)
+         {
+         System.out.println("Conexion a base de datos " + db + ". Lista!!!!");
+         }
+        
+        }catch(SQLException e){
+          System.out.println(e);
+        }catch(ClassNotFoundException e)
+                {
+                System.out.println(e);
+                }
+    
+    }
+    public Connection getConnection(){
+    return conn;
+    }
+    
+    public void desconectar(){
+    conn=null;
+    }
+
+}
